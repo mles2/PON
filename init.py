af6ed775c5f136102c0efc4405090b2dcee3ca51
Michał Leś <michalles3@gmail.com>
2018-11-03 21:40:39 +0100

    added neural net class for experiment handling

--- init.py
+++ init.py
@@ -38,79 +38,75 @@ MEANING_PARAMETER = 5
 #T - macierz diagnoz
 #teach - epoki uczenia
 #wykonanie kodu zwiazanego z siecia
-def make_experiment_with_one_layer(atr, count, P, T, teach, beta, WspUcz):
+def make_experiment_with_one_layer(atr, count, P, T, teach, beta, learning_factor):
     sr = 0
     for s in range (0, MEANING_PARAMETER):
-        Wprzed = init(atr, count)
+        neural_net = NeuralNet(atr, count)
         sk = 0
         for i in range(0,CROSSVALIDATION_PARAMETER):
             aUcz, aTest, dUcz, dTest = create_training_and_testing_data(P,T,i,int(P.shape[1]/CROSSVALIDATION_PARAMETER))
-            Wpo = ucz(Wprzed, aUcz, dUcz, teach, beta, WspUcz)
-            Ypo = dzialaj(Wpo, aTest, beta)
-            sk += accuracy(dTest,Ypo) 
+            neural_net.learn(aUcz, dUcz, teach, beta, learning_factor)
+            neural_net_result = neural_net.calculate(aTest)
+            sk += accuracy(dTest,neural_net_result) 
         sr += sk/CROSSVALIDATION_PARAMETER
+        print("Network accuracy: ", sk/CROSSVALIDATION_PARAMETER)
     worksheet.write(actual_row, actual_column, sr/MEANING_PARAMETER)
-    print(sr/MEANING_PARAMETER)
-    return Wpo
+    print("Mean accuracy: ", sr/MEANING_PARAMETER)
 
-
-
-def make_experiment_with_one_layer_with_bias(atr, count, P, T, teach, beta, WspUcz):
+def make_experiment_with_one_layer_with_bias(atr, count, P, T, teach, beta, learning_factor):
     sr = 0
     for s in range (0, MEANING_PARAMETER):
-        Wprzed = initBias(atr, count)
+        neural_net = NeuralNet(atr,count, 0, True)
         sk = 0
         for i in range(0,CROSSVALIDATION_PARAMETER):
             aUcz, aTest, dUcz, dTest = create_training_and_testing_data(P,T,i,int(P.shape[1]/CROSSVALIDATION_PARAMETER))
-            Wpo = uczBias(Wprzed, aUcz, dUcz, teach, beta, WspUcz)
-            Ypo = np.zeros((2,aTest.shape[1]))
+            neural_net.learn(aUcz, dUcz, teach, beta, learning_factor)
+            neural_net_result = np.zeros((2,aTest.shape[1]))
             for j in range(0,aTest.shape[1]):
-                Y2po = dzialajBias(Wpo,np.asmatrix(aTest[:,int(j)]).T, beta)
-                Ypo[0][j] = Y2po[0][0]
-                Ypo[1][j] = Y2po[1][0]
-            sk += accuracy(dTest,np.asmatrix(Ypo)) 
+                Y2po = neural_net.calculate(np.asmatrix(aTest[:,int(j)]).T)
+                neural_net_result[0][j] = Y2po[0][0]
+                neural_net_result[1][j] = Y2po[1][0]
+            sk += accuracy(dTest,np.asmatrix(neural_net_result)) 
         sr += sk/CROSSVALIDATION_PARAMETER
+        print("Network accuracy: ", sk/CROSSVALIDATION_PARAMETER)
 
     worksheet.write(actual_row, actual_column, sr/MEANING_PARAMETER)
-    print(sr/MEANING_PARAMETER)
-    return Wpo
+    print("Mean accuracy: ", sr/MEANING_PARAMETER)
 
 
-def make_experiment_with_two_layers(atr, count, P, T, teach, hidden, beta, WspUcz):
+def make_experiment_with_two_layers(atr, count, P, T, teach, hidden, beta, learning_factor):
     sr = 0
     for s in range (0, MEANING_PARAMETER):
-        W1przed,W2przed = init2(atr,hidden,count)
-        
+        neural_net = NeuralNet(atr, count, hidden)
         sk = 0
         for i in range(0,CROSSVALIDATION_PARAMETER):
             aUcz, aTest, dUcz, dTest = create_training_and_testing_data(P,T,i,int(P.shape[1]/CROSSVALIDATION_PARAMETER))
-            W1po, W2po = ucz2(W1przed, W2przed, aUcz, dUcz, teach, beta, WspUcz)
-            Y2po = dzialaj2(W1po, W2po, aTest, beta)[1]
-            sk += accuracy(dTest,Y2po) 
+            neural_net.learn(aUcz, dUcz, teach, beta, learning_factor)
+            neural_net_result = neural_net.calculate(aTest)[1]
+            sk += accuracy(dTest,neural_net_result) 
         sr += sk/CROSSVALIDATION_PARAMETER
+        print("Network accuracy: ", sk/CROSSVALIDATION_PARAMETER)
     worksheet.write(actual_row, actual_column, sr/MEANING_PARAMETER)
-    print(sr/MEANING_PARAMETER)
-    return W1po, W2po
+    print("Mean accuracy: ", sr/MEANING_PARAMETER)
 
-def make_experiment_with_two_layers_with_bias(atr, count, P, T, teach, hidden, beta, WspUcz):
+def make_experiment_with_two_layers_with_bias(atr, count, P, T, teach, hidden, beta, learning_factor):
     sr = 0
     for s in range (0, MEANING_PARAMETER):
-        W1przed,W2przed = init2bias(atr,hidden,count)
-        
+        neural_net = NeuralNet(atr,count, hidden, True)
         sk = 0
         for i in range(0,CROSSVALIDATION_PARAMETER):
             aUcz, aTest, dUcz, dTest = create_training_and_testing_data(P,T,i,int(P.shape[1]/CROSSVALIDATION_PARAMETER))
-            W1po, W2po = ucz2bias(W1przed,W2przed,aUcz, dUcz,teach, beta, WspUcz)
-            Ypo = np.zeros((2,aTest.shape[1]))
+            neural_net.learn(aUcz, dUcz, teach, beta, learning_factor)
+            neural_net_result = np.zeros((2,aTest.shape[1]))
             for j in range(0,aTest.shape[1]):
-                Y2po = dzialaj2bias(W1po, W2po, np.asmatrix(aTest[:,int(j)]).T, beta)[1]
-                Ypo[0][j] = Y2po[0][0]
-                Ypo[1][j] = Y2po[1][0]
-            sk += accuracy(dTest,np.asmatrix(Ypo)) 
+                Y2po = neural_net.calculate(np.asmatrix(aTest[:,int(j)]).T)[1]
+                neural_net_result[0][j] = Y2po[0][0]
+                neural_net_result[1][j] = Y2po[1][0]
+            sk += accuracy(dTest,np.asmatrix(neural_net_result)) 
         sr += sk/CROSSVALIDATION_PARAMETER
+        print("Network accuracy: ", sk/CROSSVALIDATION_PARAMETER)
     worksheet.write(actual_row, actual_column, sr/MEANING_PARAMETER)
     print(sr/MEANING_PARAMETER)
-    return W1po, W2po
 
 #T - porprawne odpowiedzi
 #Ypo - uzyskane odpowiedzi
@@ -126,8 +122,6 @@ def accuracy(T,Ypo):
         if(abs(t1-y1) < 0.4 and abs(t2-y2) < 0.4):
             poprawne += 1
     return (poprawne/ilosc)*100
-    
-    
 
 #macierz atrybutów, macierz diagnoz, numer iteracji, wielkosc przedziału out
 def create_training_and_testing_data(atr_array,diag_array, iter, zakres):
@@ -188,12 +182,12 @@ for t_epokiuczenia in T_NUMBER_OF_EPOCHS:
 
             actual_column=ONE_LAYER_RESULT_COLUMN
 
-            Wpo = make_experiment_with_one_layer(atr_array.shape[0],diag_array.shape[0],atr_array,diag_array,t_epokiuczenia, t_beta, t_wspucz)
-            #nowa_diagnoza = dzialaj(Wpo,nowy_pacjent, beta)
+            make_experiment_with_one_layer(atr_array.shape[0],diag_array.shape[0],atr_array,diag_array,t_epokiuczenia, t_beta, t_wspucz)
+            #nowa_diagnoza = calculate_one_layer_net_output(Wpo,nowy_pacjent, beta)
             actual_column+=1
 
-            Wpo = make_experiment_with_one_layer_with_bias(atr_array.shape[0],diag_array.shape[0],atr_array,diag_array,t_epokiuczenia, t_beta, t_wspucz)
-            #nowa_diagnoza = dzialajBias(Wpo,nowy_pacjent, beta)
+            make_experiment_with_one_layer_with_bias(atr_array.shape[0],diag_array.shape[0],atr_array,diag_array,t_epokiuczenia, t_beta, t_wspucz)
+            #nowa_diagnoza = calculate_one_layer_net_with_bias_output(Wpo,nowy_pacjent, beta)
             actual_column+=1
 
             for t_wu in T_NEURONS_IN_HIDDEN_LAYER:
@@ -201,12 +195,12 @@ for t_epokiuczenia in T_NUMBER_OF_EPOCHS:
                 worksheet.write(actual_row, BETA_COLUMN, t_beta)
                 worksheet.write(actual_row, LEARNING_FACTOR_COLUMN, t_wspucz)
                 worksheet.write(actual_row, EPOCHS_COLUMN, t_epokiuczenia)
-                Wpo, W2po = make_experiment_with_two_layers(atr_array.shape[0],diag_array.shape[0],atr_array,diag_array,t_epokiuczenia, t_wu, t_beta, t_wspucz)
-            # a, nowa_diagnoza = dzialaj2(Wpo, W2po, nowy_pacjent, beta)
+                make_experiment_with_two_layers(atr_array.shape[0], diag_array.shape[0], atr_array,diag_array, t_epokiuczenia, t_wu, t_beta, t_wspucz)
+            # a, nowa_diagnoza = calculate_two_layers_net_output(Wpo, W2po, nowy_pacjent, beta)
                 actual_column+=1
 
-                Wpo, W2po = make_experiment_with_two_layers_with_bias(atr_array.shape[0],diag_array.shape[0],atr_array,diag_array,t_epokiuczenia, t_wu, t_beta, t_wspucz)
-            # a, nowa_diagnoza = dzialaj2bias(Wpo, W2po, nowy_pacjent, beta)
+                make_experiment_with_two_layers_with_bias(atr_array.shape[0],diag_array.shape[0],atr_array,diag_array,t_epokiuczenia, t_wu, t_beta, t_wspucz)
+            # a, nowa_diagnoza = calculate_two_layers_net_with_bias_output(Wpo, W2po, nowy_pacjent, beta)
 
                 actual_row+=1
                 actual_column=TWO_LAYERS_RESULT_COLUMN
@@ -214,3 +208,4 @@ for t_epokiuczenia in T_NUMBER_OF_EPOCHS:
             actual_column=ONE_LAYER_RESULT_COLUMN
 
 workbook.close()
+
--- neural_net.py
+++ neural_net.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+ONE_LAYER = 1
+ONE_LAYER_BIAS = 2
+TWO_LAYERS = 3
+TWO_LAYERS_BIAS = 4
+
 #inicjacja sieci losowymi wagami
 def init(S, K):
     W1 = -0.1 + 0.2 * np.random.rand(S, K)
@@ -12,33 +17,33 @@ def initBias(S,K):
 def init2(S,K1,K2):
     W1 = -0.1 + 0.2 * np.random.rand(S, K1)
     W2 = -0.1 + 0.2 * np.random.rand(K1, K2)
-    return W1, W2
+    return [W1, W2]
 
 def init2bias(S,K1,K2):
     W1 = -0.1 + 0.2 * np.random.rand(S+1, K1)
     W2 = -0.1 + 0.2 * np.random.rand(K1+1, K2)
-    return W1, W2
+    return [W1, W2]
 
 #obliczenie odpowiedzi sieci
-def dzialaj(W, X, beta):
+def calculate_one_layer_net_output(W, X, beta):
     U = np.transpose(W) @ X
     Y = 1 / (1 + np.exp(-beta * U))
     return Y
 
-def dzialajBias(W,X, beta):
+def calculate_one_layer_net_with_bias_output(W,X, beta):
     X1 = np.append(X,[[1]],0)
     U = np.transpose(W) @ X1
     Y = 1 / (1 + np.exp(-beta * U))
     return Y
 
-def dzialaj2(W1, W2, X, beta):
+def calculate_two_layers_net_output(W1, W2, X, beta):
     U1 = np.transpose(W1) @ X
     Y1 = 1 / (1 + np.exp(-beta * U1))
     U2 = np.transpose(W2) @ Y1
     Y2 = 1 / (1 + np.exp(-beta * U2))
     return Y1, Y2
 
-def dzialaj2bias(W1, W2, X, beta):
+def calculate_two_layers_net_with_bias_output(W1, W2, X, beta):
     X1 = np.append(X,[[1]],0)
     U1 = np.transpose(W1) @ X1
     Y1 = 1 / (1 + np.exp(-beta * U1))
@@ -49,74 +54,137 @@ def dzialaj2bias(W1, W2, X, beta):
 
 
 #proces uczenia sieci
-def ucz(Wprzed, P, T, n, beta, WspUcz):
-    liczbaPrzykladow = P.shape[1]
-    W = Wprzed
-    for i in range(0, n):
+def learn_one_layer_network(weights_matrix, input_matrix, output_matrix, number_of_epochs, beta, learning_factor):
+    liczbaPrzykladow = input_matrix.shape[1]
+    W = weights_matrix
+    for i in range(0, number_of_epochs):
         nrPrzykladu = np.ceil(np.random.rand() * liczbaPrzykladow-1)
-        X = np.matrix(P[:,int(nrPrzykladu)])
+        X = np.matrix(input_matrix[:,int(nrPrzykladu)])
         X = X.T 
-        Y = dzialaj(W, X, beta)
-        D = T[:,int(nrPrzykladu)] - Y
+        Y = calculate_one_layer_net_output(W, X, beta)
+        D = output_matrix[:,int(nrPrzykladu)] - Y
         E = D* beta * Y.T * (1 - Y)
-        dW = WspUcz * X * E.T
+        dW = learning_factor * X * E.T
         W = W + dW
     Wpo = W
     return Wpo
 
-def uczBias(Wprzed, P, T, n, beta, WspUcz):
-    liczbaPrzykladow = P.shape[1]
-    W = Wprzed
-    for i in range(0, n):
+def learn_one_layer_bias_network(weights_matrix, input_matrix, output_matrix, number_of_epochs, beta, learning_factor):
+    liczbaPrzykladow = input_matrix.shape[1]
+    W = weights_matrix
+    for i in range(0, number_of_epochs):
         nrPrzykladu = np.ceil(np.random.rand() * liczbaPrzykladow-1)
-        X = np.matrix(P[:,int(nrPrzykladu)])
+        X = np.matrix(input_matrix[:,int(nrPrzykladu)])
         X1 = np.append(X,[[1]],1)
         X = X.T 
-        Y = dzialajBias(W, X, beta)
-        D = T[:,int(nrPrzykladu)] - Y
+        Y = calculate_one_layer_net_with_bias_output(W, X, beta)
+        D = output_matrix[:,int(nrPrzykladu)] - Y
         E = D* beta * Y.T * (1 - Y)
-        dW = WspUcz * X1.T * E.T
+        dW = learning_factor * X1.T * E.T
         W = W + dW
     Wpo = W
     return Wpo
 
-def ucz2(W1przed, W2przed, P, T, n, beta, WspUcz):
-    liczbaPrzykladow = P.shape[1]
-    W1 = W1przed
-    W2 = W2przed
-    for i in range(0,n):
+def learn_two_layers_network(first_layer_matrix, second_layer_matrix, input_matrix, output_matrix, number_of_epochs, beta, learning_factor):
+    liczbaPrzykladow = input_matrix.shape[1]
+    W1 = first_layer_matrix
+    W2 = second_layer_matrix
+    for i in range(0,number_of_epochs):
         nrPrzykladu = np.ceil(np.random.rand() * liczbaPrzykladow-1)
-        X1 = np.matrix(P[:,int(nrPrzykladu)])
+        X1 = np.matrix(input_matrix[:,int(nrPrzykladu)])
         X1 = X1.T 
-        Y1,Y2 = dzialaj2(W1, W2, X1, beta)
-        D2 = T[:,int(nrPrzykladu)] - Y2
+        Y1,Y2 = calculate_two_layers_net_output(W1, W2, X1, beta)
+        D2 = output_matrix[:,int(nrPrzykladu)] - Y2
         E2 = D2* beta * Y2.T * (1 - Y2)
         D1 = W2 * E2
         E1 =  D1* beta * Y1.T * (1 - Y1)
-        dW1 = WspUcz * X1 * E1.T
-        dW2 = WspUcz * Y1 * E2.T
+        dW1 = learning_factor * X1 * E1.T
+        dW2 = learning_factor * Y1 * E2.T
         W1 = W1 + dW1
         W2 = W2 + dW2
     return W1,W2
 
-def ucz2bias(W1przed, W2przed, P, T, n, beta, wspUcz):
-    liczbaPrzykladow = P.shape[1]
-    W1 = W1przed
-    W2 = W2przed
-    for i in range(0,n):
+def learn_two_layers_bias_network(first_layer_matrix, second_layer_matrix, input_matrix, output_matrix, number_of_epochs, beta, learning_factor):
+    liczbaPrzykladow = input_matrix.shape[1]
+    W1 = first_layer_matrix
+    W2 = second_layer_matrix
+    for i in range(0,number_of_epochs):
         nrPrzykladu = np.ceil(np.random.rand() * liczbaPrzykladow-1)
-        X = np.matrix(P[:,int(nrPrzykladu)])
+        X = np.matrix(input_matrix[:,int(nrPrzykladu)])
         X1 = np.append(X,[[1]],1)
         X = X.T 
-        Y1,Y2 = dzialaj2bias(W1, W2, X, beta)
+        Y1,Y2 = calculate_two_layers_net_with_bias_output(W1, W2, X, beta)
         X2 = np.append(Y1,[[1]],0)
-        D2 = T[:,int(nrPrzykladu)] - Y2
+        D2 = output_matrix[:,int(nrPrzykladu)] - Y2
         E2 = D2* beta * Y2.T * (1 - Y2)
         D1 = W2[:-1,:] * E2
         E1 =  D1* beta * Y1.T * (1 - Y1)
-        dW1 = wspUcz * X1.T * E1.T
-        dW2 = wspUcz * X2 * E2.T
+        dW1 = learning_factor * X1.T * E1.T
+        dW2 = learning_factor * X2 * E2.T
         W1 = W1 + dW1
         W2 = W2 + dW2
     return W1,W2
-        
+
+class NeuralNet:
+    def __init__(self,
+                 number_of_inputs,
+                 number_of_outputs,
+                 neurons_in_hidden_layer = 0,
+                 bias = False):
+
+        self.number_of_inputs = number_of_inputs
+        self.number_of_outputs = number_of_outputs
+        self.neurons_in_hidden_layer = neurons_in_hidden_layer
+        self.bias = bias
+
+        print("Trying to create neural net with parameters:")
+        self.display_parameters()
+
+        if((neurons_in_hidden_layer == 0) and (bias == False)):
+            self.weight_matrixes = [init(number_of_inputs, number_of_outputs)]
+            self.net_type = ONE_LAYER
+        elif((neurons_in_hidden_layer == 0) and (bias == True)):
+            self.weight_matrixes = [initBias(number_of_inputs, number_of_outputs)]
+            self.net_type = ONE_LAYER_BIAS
+        elif((neurons_in_hidden_layer > 0) and (bias == False)):
+            self.weight_matrixes = init2(number_of_inputs, neurons_in_hidden_layer, number_of_outputs)
+            self.net_type = TWO_LAYERS
+        elif((neurons_in_hidden_layer > 0) and (bias == True)):
+            self.weight_matrixes = init2bias(number_of_inputs, neurons_in_hidden_layer, number_of_outputs)
+            self.net_type = TWO_LAYERS_BIAS
+        else:
+            print("Problem with initiating network!")
+
+    def learn(self, input_matrix, output_matrix, number_of_epochs, beta, learning_factor):
+        self.beta = beta
+        if(self.net_type == ONE_LAYER):
+            self.weight_matrixes[0] = learn_one_layer_network(self.weight_matrixes[0],input_matrix, output_matrix, number_of_epochs, beta, learning_factor)
+        elif(self.net_type == ONE_LAYER_BIAS):
+            self.weight_matrixes[0] = learn_one_layer_bias_network(self.weight_matrixes[0], input_matrix, output_matrix, number_of_epochs, beta, learning_factor)
+        elif(self.net_type == TWO_LAYERS):
+            self.weight_matrixes[0], self.weight_matrixes[1] = learn_two_layers_network(self.weight_matrixes[0], self.weight_matrixes[1], input_matrix, output_matrix, number_of_epochs, beta, learning_factor)
+        elif(self.net_type == TWO_LAYERS_BIAS):
+            self.weight_matrixes[0], self.weight_matrixes[1] = learn_two_layers_bias_network(self.weight_matrixes[0], self.weight_matrixes[1], input_matrix, output_matrix, number_of_epochs, beta, learning_factor)
+
+    def calculate(self, input_vector):
+        if(self.net_type == ONE_LAYER):
+            return calculate_one_layer_net_output(self.weight_matrixes[0],input_vector, self.beta)
+        elif(self.net_type == ONE_LAYER_BIAS):
+            return calculate_one_layer_net_with_bias_output(self.weight_matrixes[0], input_vector, self.beta)
+        elif(self.net_type == TWO_LAYERS):
+            return calculate_two_layers_net_output(self.weight_matrixes[0], self.weight_matrixes[1], input_vector, self.beta)
+        elif(self.net_type == TWO_LAYERS_BIAS):
+            return calculate_two_layers_net_with_bias_output(self.weight_matrixes[0], self.weight_matrixes[1], input_vector, self.beta)
+
+
+    def display_parameters(self):
+        print("Number of inputs: ", self.number_of_inputs)
+        print("Number of outputs: ", self.number_of_outputs)
+        if(self.neurons_in_hidden_layer > 0):
+            print("Number of neurons in hidden layer: ", self.neurons_in_hidden_layer)
+        print("Bias: ", self.bias)
+
+neural_net_2 = NeuralNet(2,4)
+neural_net_1 = NeuralNet(2,3,4)
+neural_net_3 = NeuralNet(2,4,0,True)
+neural_net_3 = NeuralNet(2,4,2,True)
